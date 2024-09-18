43fde2e4f4505a4291b073e0c2a79236a8552987
integral_digi <ojaduagreat@gmail.com>
2024-09-18 20:43:04 +0100

    Checkbox workaround

--- app/components/TagBar.tsx
+++ app/components/TagBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Checkbox } from "@headlessui/react";
+import CheckboxOne from "@/public/assets/CheckBox";
 
 const TagBar: React.FC = () => {
   const [globalTagsChecked, setGlobalTagsChecked] = useState(false);
@@ -23,18 +24,7 @@ const TagBar: React.FC = () => {
                 } flex items-center justify-center`}
               >
                 {checked && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M16.707 5.293a1 1 0 00-1.414-1.414L7 12.586 4.707 10.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l9-9z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                    <CheckboxOne />
                 )}
               </div>
             )}
@@ -58,18 +48,7 @@ const TagBar: React.FC = () => {
                 } flex items-center justify-center`}
               >
                 {checked && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M16.707 5.293a1 1 0 00-1.414-1.414L7 12.586 4.707 10.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l9-9z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                    <CheckboxTwo />
                 )}
               </div>
             )}
---
+++ public/assets/CheckBox.tsx
@@ -0,0 +1,18 @@
+const CheckboxOne = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 text-white"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+        >
+            <path
+                fillRule="evenodd"
+                d="M16.707 5.293a1 1 0 00-1.414-1.414L7 12.586 4.707 10.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l9-9z"
+                clipRule="evenodd"
+            />
+        </svg>
+    )
+}
+
+export default CheckboxOne;
